c082578291aef7e38f9b9a661be997a930d5c9ac
hktk7 <hktk7@126.com>
2025-04-25 12:14:57 +0800

    20250425

---
+++ link.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+cd update
+ln -s yhzb.apk atv.apk
+ln -s yhzb.apk azb.apk
+ln -s yhzb.apk bhzb.apk
+ln -s yhzb.apk bttv.apk
+ln -s yhzb.apk btzb.apk
+ln -s yhzb.apk bstv.apk
+ln -s yhzb.apk cjgq.apk
+ln -s yhzb.apk cjwn.apk
+ln -s yhzb.apk cztv.apk
+ln -s yhzb.apk cjiptv.apk
+ln -s yhzb.apk cjzb.apk
+ln -s yhzb.apk dzzb.apk
+ln -s yhzb.apk iptv.apk
+ln -s yhzb.apk iptvs.apk
+ln -s yhzb.apk iptvd.apk
+ln -s yhzb.apk itv.apk
+ln -s yhzb.apk kds.apk
+ln -s yhzb.apk kmds.apk
+ln -s yhzb.apk lxzb.apk
+ln -s yhzb.apk mkds.apk
+ln -s yhzb.apk mgzb.apk
+ln -s yhzb.apk mkwlzb.apk
+ln -s yhzb.apk sszb.apk
+ln -s yhzb.apk ttzb.apk
+ln -s yhzb.apk wlzb.apk
+ln -s yhzb.apk xhzb.apk
+ln -s yhzb.apk xhds.apk
+ln -s yhzb.apk xyzb.apk
+ln -s yhzb.apk yuzb.apk
+ln -s yhzb.apk yszb.apk
+ln -s ../update apk
+ln -s ../update up
+ln -s ../update update
+
+cd ../
+ln -s dsch/ipv4 kds
+ln -s dsch/ipv4 ipv6
+ln -s dsch/ipv4 tvch
+ln -s dsch/ipv4 xhch
+ln -s dsch/ipv4 ipv4
+
+ln -s update apk
+ln -s update up
+
+ln -s update/yhzb.apk cjgq.apk
+ln -s update/yhzb.apk cjwn.apk
+ln -s update/yhzb.apk iptv.apk
+ln -s update/yhzb.apk kmds.apk
